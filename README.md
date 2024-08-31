621dcc7aa8d2498204137321e0f42dec3e7b6e7a
krishna <kv510341@gmail.com>
2024-08-31 15:36:48 +0530

    Home Page UI completed

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 - we are going to use shadCn starter to initialize our project.
+
+# creating Home Page
+
+- We are going to use `Intl.NumberFormat` function to format our currency
+- we are going to use `react-countup` package for countup effect.
+
+- for charts we are going to use `chart.js` and `react-chartjs-2`.
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,28 @@
+import HeaderBox from "@/components/shared/HeaderBox";
+import TotalBalanceBox from "@/components/shared/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Karan" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
